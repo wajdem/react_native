37b737b8b0026fc4768d576a9fa4ea13bd8388c6
wajdem <wajdi.abuarqob@gmail.com>
2023-08-16 22:22:01 +0300

    done

--- name/screens/Exercise2.js
+++ name/screens/Exercise2.js
@@ -1,32 +1,35 @@
-import React from 'react'
-import { View,Text,StyleSheet } from 'react-native'
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from "react-native";
 
 const Exercise2 = () => {
+  const handleButtonPress = () => {
+    Alert.alert("Hello", "You tapped the button!");
+  };
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Hello World!</Text>
-      </View>
+      <TouchableOpacity onPress={handleButtonPress}>
+        <Text
+          style={{
+            padding: 10,
+            backgroundColor: "blue",
+            color: "white",
+            textAlign: "center",
+          }}
+        >
+          Tap Me
+        </Text>
+      </TouchableOpacity>
     </View>
-  )
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "gray",
-  },
-  box: {
-    height: 100,
-    width: 100,
-    backgroundColor: "blue",
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text:{
-    color: "white",
-  }
 })
 
-export default Exercise2
+export default Exercise2;
