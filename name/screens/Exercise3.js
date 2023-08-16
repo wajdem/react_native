6faa828f43488830b81b2eb3d01840de013d7209
wajdem <wajdi.abuarqob@gmail.com>
2023-08-16 22:23:13 +0300

    done

--- name/screens/Exercise3.js
+++ name/screens/Exercise3.js
@@ -1,32 +1,39 @@
-import React from 'react'
-import { View,Text,StyleSheet } from 'react-native'
+import React from "react";
+import { View, Text, Alert, StyleSheet } from "react-native";
+import Button from "../components/Button";
 
 const Exercise3 = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Hello World!</Text>
-      </View>
-    </View>
-  )
-}
+      <Button
+        onPress={
+          (handleButtonPress = () => {
+            Alert.alert("Hello");
+          })
+        }
+        text="Say Hello"
+      />
 
+      <Button
+        onPress={
+          (handleButtonPress = () => {
+            Alert.alert("Goodbye");
+          })
+        }
+        text="Say Goodbye"
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "gray",
-  },
-  box: {
-    height: 100,
-    width: 100,
-    backgroundColor: "blue",
+    gap: 15,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text:{
-    color: "white",
-  }
 })
 
-export default Exercise3
+export default Exercise3;
